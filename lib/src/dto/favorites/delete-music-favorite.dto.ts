de64e395325b4dcddd22bc8f4d20bbe1569d923f
jhonatan.gomes@webjump.com.br <jhonatan.gomes@webjump.com.br>
2026-01-28 21:12:04 -0300

    Creating endpoint to delete favorite music

---
+++ lib/src/dto/favorites/delete-music-favorite.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class DeleteFavoriteMusicDto {
+    @ApiProperty({ type: 'string' })
+    @IsString()
+    @IsNotEmpty()
+    user_id: string;
+
+    @ApiProperty({ type: 'string' })
+    @IsString()
+    @IsNotEmpty()
+    music_id: string;
+}
